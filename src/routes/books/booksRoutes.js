b70c3159bd295c3b5dc062071ebe336341f8d3ee
VictorAdmin <victorescalona2006@gmail.com>
2024-12-26 14:06:33 -0400

    agregue las validaciones a registrar usuarios y al agregar libros

--- src/routes/books/booksRoutes.js
+++ src/routes/books/booksRoutes.js
@@ -2,6 +2,8 @@ import { Router } from "express";
 import booksAdmin from "../../controllers/books/index.js";
 import authenticateTokenAdmin from "../../middlewares/verifyTokenAdmin.js";
 import authenticateTokenUser from "../../middlewares/verifyTokenUser.js";
+import { bookValidationRules } from "../../middlewares/dataFilter.js";
+import {validateBooks} from "../../middlewares/dataValidation.js";
 
 const router = Router();
 
@@ -10,7 +12,7 @@ router.get("/delete", authenticateTokenAdmin, booksAdmin.renderDeleteBook);
 router.get("/search", authenticateTokenUser, booksAdmin.renderBookSearch);
 router.get("/:id", authenticateTokenUser, booksAdmin.renderBook);
 
-router.post("/add", authenticateTokenAdmin, booksAdmin.addBook);
+router.post("/add", authenticateTokenAdmin, bookValidationRules, validateBooks, booksAdmin.addBook);
 router.get("/", booksAdmin.getBooks);
 router.post("/advanced", booksAdmin.getBooksAdvanced);
 router.put("/update", authenticateTokenAdmin, booksAdmin.updateBook);
